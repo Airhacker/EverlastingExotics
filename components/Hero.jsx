87714821267916fc6431c0adf71fd8f807035165
Sigword <ethan.soto@yahoo.com>
2022-11-01 21:33:10 -0500

    Hero page rough draft

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,18 @@
 import styles from "../styles/hero.module.css";
+import Image from "next/image";
+import Mclaren from "../public/Mclaren.png";
+import Link from "next/link";
 
 export default function Hero() {
   return (
     <section className={styles.root}>
       <div className={styles.Herotext}>Everlasting Exotics</div>
-      <div className={styles.HeroCar}></div>
-      <div className={styles.HeroButton}></div>
+      <div className={styles.HeroCar}>
+        <Image src={Mclaren} alt="Mclaren" width={1300} height={866} />
+      </div>
+      <div className={styles.HeroButton}>
+        <a>Open Catalog</a>
+      </div>
     </section>
   );
 }
